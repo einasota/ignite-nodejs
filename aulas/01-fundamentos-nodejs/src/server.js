3a5397be0276dcd6c95d4b62ba502e0ffd2ec72a
Jhonata Souza <einasota@gmail.com>
2025-06-16 09:21:48 -0300

    Adicionando Comentários

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,5 +1,29 @@
 import http from 'node:http'
 
+// - Criar usuário
+// - Listagem de usuários
+// - Edição de usuários
+// - Remoção de usuários 
+
+// - HTTP
+//  - Método HTTP
+//  - URL
+
+// GET, POST, PUT, PATCH, DELETE 
+
+// GET => Buscar um recurso no back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação especifica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuário do back-end
+// POST /users => Criar um usuário do back-end
+//
+// Stateful - Stateless
+//
+// Cabeçalhos (Requisição/Resposta) => Metadados
+
 const users = []
 
 const server = http.createServer((req, res) => {
